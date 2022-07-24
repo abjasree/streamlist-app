dee84e60945248b22df338162b644d0247933c1d
Abjasree <83914220+abjasree@users.noreply.github.com>
2022-07-24 22:05:56 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"22ds1000214@student.onlinedegree.iitm.ac.in\"\n\
+email = \"abjasree1@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
